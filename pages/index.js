f00c00aecfa314c048459564165c0900e214c504
Nevyl <christiannevyl@MacBook-Pro-de-ChristianNevyl.local>
2022-07-09 14:16:21 -0500

    media commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React from "react";
-import YouTube from "react-youtube";
 
 export default function Home() {
   return (
